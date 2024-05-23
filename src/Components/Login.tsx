a86fffe687280eac854df79c373ac2cc31858489
leem-dev <alabitseleem@gmail.com>
2024-05-23 09:11:36 +0100

    get the value of inputs using useState hook

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -4,6 +4,19 @@ import Button from "./Button";
 
 const Login = () => {
   const [login, setLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = () => {
+    const data = { email, password, confirmPassword };
+    console.log(data);
+  };
+
+  const handleSignin = () => {
+    const data = { email, password };
+    console.log(data);
+  };
 
   return (
     <div className="m-6 w-full md:w-[450px]">
@@ -11,19 +24,36 @@ const Login = () => {
         {login ? "Login" : "Register"}
       </h1>
       <div className="flex flex-col gap-3 bg-white w-full p-6 min-h-[150px] rounded-xl drop-shadow-xl">
-        <Input name="email" type="email" />
-        <Input name="password" type="password" />
-        {!login && <Input name="Confirm password" type="password" />}
+        <Input
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {!login && (
+          <Input
+            name="Confirm password"
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
 
         {/* Switch button when necessary */}
         {login ? (
           <>
-            <Button text="Login" />
+            <Button text="Login" onClick={handleSignin} />
             <Button onClick={() => setLogin(false)} text="Register" secondary />
           </>
         ) : (
           <>
-            <Button text="Register" />
+            <Button text="Register" onClick={handleSignup} />
             <Button onClick={() => setLogin(true)} text="Login" secondary />
           </>
         )}
