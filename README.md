102a4238350b7e2f7a9038fcb52ec99ac334177c
leem-dev <alabitseleem@gmail.com>
2024-05-24 12:40:26 +0100

    installing Redux for managing state

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 uChat
+
+# TYPESCRIPT
+
+# TAILWIND CSS
+
+# FIREBASE
+
+- Authentication
+- Cloud Firestore
+
+# REDUX
+
+- A production way of managing state
---
+++ src/Redux/userSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+    setUsers: (state, action) => {},
+  },
+});
