d61f83737cd94623edb8291a073f6059b20867a4
leem-dev <alabitseleem@gmail.com>
2024-05-31 12:17:11 +0100

    working with routes on header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -20,9 +20,23 @@ function Header() {
   const dispatch = useDispatch();
   const currentUser = useSelector((state: RootState) => state.user.currentUser);
 
+  const handleGoToPage = (page: string) => {
+    goTo("/dashboard/" + page);
+    setCurrentPage(page);
+  };
+
   const handleSignOut = () => {
     BE_signOut(dispatch, goTo, setLogoutLoading);
   };
+
+  const setCurrentPage = (page: string) => {
+    localStorage.setItem("new-UserPage", page);
+  };
+
+  const getCurrentPage = () => {
+    return localStorage.getItem("new-UserPage");
+  };
+
   return (
     <div className="flex flex-wrap sm:flex-row gap-5 items-center justify-between drop-shadow-md bg-gradient-to-r from-myBlue to-myPink px-5 py-5 md:py-2 text-white">
       <img
@@ -31,19 +45,39 @@ function Header() {
         alt="img"
       />
       <div className="flex flex-row-reverse md:flex-row items-center justify-center gap-5 flex-wrap">
-        <AddListBoard />
-        <Icon IconName={BsFillChatFill} ping={true} />
-        <Icon IconName={FiList} />
+        {getCurrentPage() === "chat" ? (
+          <Icon IconName={FiList} onClick={() => handleGoToPage("list")} />
+        ) : getCurrentPage() === "profile" ? (
+          <>
+            <Icon IconName={FiList} onClick={() => handleGoToPage("list")} />
+
+            <Icon
+              IconName={BsFillChatFill}
+              ping={true}
+              onClick={() => handleGoToPage("chat")}
+            />
+          </>
+        ) : (
+          <>
+            <AddListBoard />
+            <Icon
+              IconName={BsFillChatFill}
+              ping={true}
+              onClick={() => handleGoToPage("chat")}
+            />
+          </>
+        )}
+
         <div className="group relative">
           <UserHeaderProfile user={currentUser} />
           <div className="absolute pt-5 hidden group-hover:block w-full min-w-max">
             <ul className="w-full bg-white overflow-hidden rounded-md shadow-md text-gray-700 pt-1">
-              <Link
-                to="/dashboard/profile"
-                className="hover:bg-gray-200 py-2 px-4 block"
+              <p
+                onClick={() => handleGoToPage("profile")}
+                className="hover:bg-gray-200 py-2 px-4 block cursor-pointer"
               >
                 Profile
-              </Link>
+              </p>
               <p
                 onClick={() => !logoutLoading && handleSignOut()}
                 className={`hover:bg-gray-200 py-2 px-4 cursor-pointer flex items-center gap-4 ${
