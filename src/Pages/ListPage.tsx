33487a7c0cb4ed4d45718525748ca3bea49b5b28
leem-dev <alabitseleem@gmail.com>
2024-06-03 10:38:14 +0100

    displaying task list

--- src/Pages/ListPage.tsx
+++ src/Pages/ListPage.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import SingleTaskList from "../Components/SingleTaskList";
 import { BE_getTaskList } from "../Backend/Queries";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../Redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
 
 type Props = {};
 
 function ListPage({}: Props) {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
+  const taskList = useSelector(
+    (state: RootState) => state.taskList.currentTaskList
+  );
 
   useEffect(() => {
     BE_getTaskList(dispatch, setLoading);
@@ -17,12 +20,9 @@ function ListPage({}: Props) {
   return (
     <div className="p-10 ">
       <div className="flex flex-wrap justify-center gap-10">
-        <SingleTaskList />
-        <SingleTaskList />
-        <SingleTaskList />
-        <SingleTaskList />
-        <SingleTaskList />
-        <SingleTaskList />
+        {taskList.map((t) => (
+          <SingleTaskList singleTaskList={t} />
+        ))}
       </div>
     </div>
   );
