2954bdee0c557428db63600657e52793cf99cf3a
leem-dev <alabitseleem@gmail.com>
2024-06-05 12:47:45 +0100

    working on textarea for description using editmode and collapsed

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import Icon from "./Icon";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { taskType } from "../Types";
@@ -14,24 +14,48 @@ const Task = forwardRef(
     ref: React.LegacyRef<HTMLDivElement> | undefined
   ) => {
     const { id, title, description, editMode, collapsed } = task;
+    const [homeTitle, setHomeTitle] = useState(title);
+    const [homeDescription, setHomeDescription] = useState(description);
+
     return (
       <div
         ref={ref}
         className="bg-white p-2 mb-2 rounded-md drop-shadow-sm hover:drop-shadow-md"
       >
         <div>
-          <p className="cursor-pointer">{title}</p>
+          {editMode ? (
+            <input
+              value={homeTitle}
+              onChange={(e) => setHomeTitle(e.target.value)}
+              className="border-2 px-2 border-myBlue rounded-sm mb-1"
+              placeholder="task title"
+            />
+          ) : (
+            <p className="cursor-pointer">{title}</p>
+          )}
         </div>
-        <div>
-          <hr />
+        {!collapsed && (
           <div>
-            <p>{description}</p>
-            <div className="flex justify-end">
-              <Icon IconName={MdEdit} />
-              <Icon IconName={MdDelete} />
+            <hr />
+            <div>
+              {editMode ? (
+                <textarea
+                  onChange={(e) => setHomeDescription(e.target.value)}
+                  value={homeDescription}
+                  placeholder="todo description"
+                  className="w-full px-3 border-2 border-myBlue rounded-md mt-2"
+                />
+              ) : (
+                <p className="p-2 text-justify">{description}</p>
+              )}
+
+              <div className="flex justify-end">
+                <Icon IconName={MdEdit} />
+                <Icon IconName={MdDelete} />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
