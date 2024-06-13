ff2b8b0385990a2618ce399c2cb4813bf7ad79ab
leem-dev <alabitseleem@gmail.com>
2024-06-13 13:13:37 +0100

    generate random avatar for the profile picture

--- src/Pages/ProfilePage.tsx
+++ src/Pages/ProfilePage.tsx
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../Components/Input";
 import Button from "../Components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
+import AvatarGenerator from "../utils/AvatarGenerator";
 
 function ProfilePage() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const [avatar, setAvatar] = useState("");
+
+  const currentUser = useSelector((state: RootState) => state.user.currentUser);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    setEmail(currentUser.email);
+    setUsername(currentUser.username);
+  }, [currentUser]);
+
+  const handleProfileAvatar = () => {
+    setAvatar(AvatarGenerator());
+  };
+
   return (
     <div className="bg-white flex flex-col gap-5 shadow-md max-w-2xl rounded-xl py-5 px-6 md:p-10 md:m-auto m-5 md:mt-10">
-      <div className="relative self-center">
+      <div className="relative self-center" onClick={handleProfileAvatar}>
         <img
-          src="https://api.multiavatar.com/test.png"
-          alt=""
+          src={avatar || currentUser.img}
+          alt={currentUser.username}
           className="w-32 h-32 md:w-48 md:h-48 rounded-full p-[2px] ring-gray-300 cursor-pointer hover:shadow-lg"
         />
         <span className="absolute top-7 md:top-7 left-28 md:left-40 w-5 h-5 border-2 border-gray-800 rounded-full bg-green-400"></span>
       </div>
-      <p className="text gray 400 text-sm text-center">
+      <p className="text-gray-400 text-sm text-center">
         Note: Click on image to temporary change it, when you like it, then save
         profile. You can leave password and username as they are if you don't
         want to change them
       </p>
       <div className="flex flex-col gap-2">
-        <Input name="email" />
-        <Input name="email" />
-        <Input name="email" />
-        <Input name="email" />
+        <Input
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value.trim())}
+        />
+        <Input
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value.trim())}
+        />
+        <Input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          value={confirmPass}
+          onChange={(e) => setConfirmPass(e.target.value)}
+        />
         <Button text="Update Profile" />
         <Button text="Delete Account" secondary />
       </div>
--- src/utils/AvatarGenerator.ts
+++ src/utils/AvatarGenerator.ts
@@ -1,4 +1,6 @@
+import { generate } from "random-words";
+
 export default function AvatarGenerator(text?: string) {
   // TODO: implement random text generator here for avatar
-  return `https://api.multiavatar.com/${text || "random"}.png`;
+  return `https://api.multiavatar.com/${text || generate()}.png`;
 }
